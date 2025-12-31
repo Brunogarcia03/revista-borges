d8ba99681b400cec2155013150c8ed681e105d27
Brunogarcia03 <brunogalberti@gmail.com>
2025-12-31 12:12:57 -0300

    vercel 3v

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  adapter: vercel({}),
-
   vite: {
     plugins: [tailwindcss()],
   },
