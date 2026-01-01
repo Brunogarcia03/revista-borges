f674ed3309c1b1e075dcfbbe04e9a9514c38f2e8
Brunogarcia03 <brunogalberti@gmail.com>
2026-01-01 17:58:03 -0300

    vercel 10v

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+
 import keystatic from "@keystatic/astro";
 
 import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    runtime: "nodejs20.x",
+  }),
 
   vite: {
     plugins: [tailwindcss()],
